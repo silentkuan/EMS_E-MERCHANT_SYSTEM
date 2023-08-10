c7b1a5921d372152298285071c3fec787302a90d
silentkuan <101119449+silentkuan@users.noreply.github.com>
2023-08-11 02:08:48 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -176,13 +176,13 @@ $(document).ready(function () {
   <script>
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBXtmwbDNWIMVyXnoGBkjrM9gggqPu3ySs",
-    authDomain: "ems-e-merchant-system.firebaseapp.com",
-    projectId: "ems-e-merchant-system",
-    storageBucket: "ems-e-merchant-system.appspot.com",
-    messagingSenderId: "895039847507",
-    appId: "1:895039847507:web:d287b9b86346beddafdaa6",
-    measurementId: "G-5ZWXT5C8S1"
+    apiKey: APIKEY,
+    authDomain: AUTHDOMAIN,
+    projectId: PROJECTID,
+    storageBucket: STORAGEBUCKET,
+    messagingSenderId: MESSAGINGSENDERID,
+    appId: APPID,
+    measurementId: MEASUREMENTID
   }; 
 
 // Initialize Firebase
